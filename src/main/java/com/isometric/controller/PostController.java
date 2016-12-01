484727d2767e7e7a179f2e624d7c54b82aa3e730
ChintanVachhani <chintan.vachhani10@gmail.com>
2016-12-01 03:33:23 -0600

    Implemented search filter functionality

--- src/main/java/com/isometric/controller/PostController.java
+++ src/main/java/com/isometric/controller/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -59,23 +60,87 @@ public class PostController {
         return postList;
     }
 
+    //Naive Implementation of Search functionality
     @CrossOrigin(origins = "http://localhost:9090")
     @RequestMapping(value = "/search", method = RequestMethod.GET)
-    public List<Post> searchPosts(@RequestParam(value = "postSearch") String postSearch) {
-        //, @RequestParam(value = "plastic") String plastic
-        //, @RequestParam(value = "glass") String glass
-        //, @RequestParam(value = "polycarbonate") String polycarbonate
-        //, @RequestParam(value = "polyamide") String polyamide,
-        //, @RequestParam(value = "S") String s
-        //, @RequestParam(value = "M") String m
-        //, @RequestParam(value = "L") String l
-        //, @RequestParam(value = "XL") String xl
-        //, @RequestParam(value = "solid") String solid
-        //, @RequestParam(value = "hollow") String hollow
-        //, @RequestParam(value = "mono") String mono
-        //, @RequestParam(value = "dual") String dual
-        //, @RequestParam(value = "multi") String multi
-        postList = postRepository.findByPostTitleLikeIgnoreCase(postSearch);
+    public List<Post> searchPosts(@RequestParam(value = "postSearch") String postSearch, @RequestParam(value = "plastic") String plastic, @RequestParam(value = "glass") String glass, @RequestParam(value = "polycarbonate") String polycarbonate, @RequestParam(value = "polyamide") String polyamide, @RequestParam(value = "S") String s, @RequestParam(value = "M") String m, @RequestParam(value = "L") String l, @RequestParam(value = "XL") String xl, @RequestParam(value = "solid") String solid, @RequestParam(value = "hollow") String hollow, @RequestParam(value = "mono") String mono, @RequestParam(value = "dual") String dual, @RequestParam(value = "multi") String multi) {
+        List<String> itemMaterial = new ArrayList<String>();
+        List<String> itemSize = new ArrayList<String>();
+        List<String> itemBuiltType = new ArrayList<String>();
+        List<String> itemColorType = new ArrayList<String>();
+        if (plastic.equals("on")) {
+            itemMaterial.add("Plastic");
+        }
+        if (glass.equals("on")) {
+            itemMaterial.add("Glass");
+        }
+        if (polycarbonate.equals("on")) {
+            itemMaterial.add("Polycarbonate");
+        }
+        if (polyamide.equals("on")) {
+            itemMaterial.add("Polyamide");
+        }
+        if (s.equals("on")) {
+            itemSize.add("S");
+        }
+        if (m.equals("on")) {
+            itemSize.add("M");
+        }
+        if (l.equals("on")) {
+            itemSize.add("L");
+        }
+        if (xl.equals("on")) {
+            itemSize.add("XL");
+        }
+        if (solid.equals("on")) {
+            itemBuiltType.add("Solid");
+        }
+        if (hollow.equals("on")) {
+            itemBuiltType.add("Hollow");
+        }
+        if (mono.equals("on")) {
+            itemColorType.add("Mono");
+        }
+        if (dual.equals("on")) {
+            itemColorType.add("Dual");
+        }
+        if (multi.equals("on")) {
+            itemColorType.add("Multi");
+        }
+        if (itemMaterial.isEmpty() && itemSize.isEmpty() && itemBuiltType.isEmpty() && itemColorType.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCase(postSearch);
+        } else if (itemMaterial.isEmpty() && itemSize.isEmpty() && itemBuiltType.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemColorTypeIn(postSearch, itemColorType);
+        } else if (itemMaterial.isEmpty() && itemSize.isEmpty() && itemColorType.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemBuiltTypeIn(postSearch, itemBuiltType);
+        } else if (itemMaterial.isEmpty() && itemBuiltType.isEmpty() && itemColorType.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemSizeIn(postSearch, itemSize);
+        } else if (itemSize.isEmpty() && itemBuiltType.isEmpty() && itemColorType.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemMaterialIn(postSearch, itemMaterial);
+        } else if (itemSize.isEmpty() && itemMaterial.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemBuiltTypeInAndItemColorTypeIn(postSearch, itemBuiltType, itemColorType);
+        } else if (itemBuiltType.isEmpty() && itemMaterial.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemSizeInAndItemColorTypeIn(postSearch, itemSize, itemColorType);
+        } else if (itemBuiltType.isEmpty() && itemSize.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemColorTypeIn(postSearch, itemMaterial, itemColorType);
+        } else if (itemColorType.isEmpty() && itemMaterial.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemSizeInAndItemBuiltTypeIn(postSearch, itemSize, itemBuiltType);
+        } else if (itemColorType.isEmpty() && itemBuiltType.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemSizeIn(postSearch, itemMaterial, itemSize);
+        } else if (itemColorType.isEmpty() && itemSize.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemBuiltTypeIn(postSearch, itemMaterial, itemBuiltType);
+        } else if (itemMaterial.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemSizeInAndItemBuiltTypeInAndItemColorTypeIn(postSearch, itemSize, itemBuiltType, itemColorType);
+        } else if (itemBuiltType.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemSizeInAndItemColorTypeIn(postSearch, itemMaterial, itemSize, itemColorType);
+        } else if (itemSize.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemBuiltTypeInAndItemColorTypeIn(postSearch, itemMaterial, itemBuiltType, itemColorType);
+        } else if (itemColorType.isEmpty()) {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemSizeInAndItemBuiltTypeIn(postSearch, itemMaterial, itemSize, itemBuiltType);
+        } else {
+            postList = postRepository.findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemSizeInAndItemBuiltTypeInAndItemColorTypeIn(postSearch, itemMaterial, itemSize, itemBuiltType, itemColorType);
+        }
+
         return postList;
     }
 }
--- src/main/java/com/isometric/repository/PostRepository.java
+++ src/main/java/com/isometric/repository/PostRepository.java
@@ -8,5 +8,36 @@ import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, BigInteger> {
     List<Post> findByUserId(BigInteger userId);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemSizeInAndItemBuiltTypeInAndItemColorTypeIn(String postTitle, List<String> itemMaterial, List<String> itemSize, List<String> itemBuiltType, List<String> itemColorType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemSizeInAndItemBuiltTypeIn(String postTitle, List<String> itemMaterial, List<String> itemSize, List<String> itemBuiltType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemBuiltTypeInAndItemColorTypeIn(String postTitle, List<String> itemMaterial, List<String> itemBuiltType, List<String> itemColorType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemSizeInAndItemColorTypeIn(String postTitle, List<String> itemMaterial, List<String> itemSize, List<String> itemColorType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemSizeInAndItemBuiltTypeInAndItemColorTypeIn(String postTitle, List<String> itemSize, List<String> itemBuiltType, List<String> itemColorType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemBuiltTypeIn(String postTitle, List<String> itemMaterial, List<String> itemBuiltType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemSizeIn(String postTitle, List<String> itemMaterial, List<String> itemSize);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemSizeInAndItemBuiltTypeIn(String postTitle, List<String> itemSize, List<String> itemBuiltType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemMaterialInAndItemColorTypeIn(String postTitle, List<String> itemMaterial, List<String> itemColorType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemSizeInAndItemColorTypeIn(String postTitle, List<String> itemSize, List<String> itemColorType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemBuiltTypeInAndItemColorTypeIn(String postTitle, List<String> itemBuiltType, List<String> itemColorType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemMaterialIn(String postTitle, List<String> itemMaterial);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemSizeIn(String postTitle, List<String> itemSize);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemBuiltTypeIn(String postTitle, List<String> itemBuiltType);
+
+    List<Post> findByPostTitleLikeIgnoreCaseAndItemColorTypeIn(String postTitle, List<String> itemColorType);
+
     List<Post> findByPostTitleLikeIgnoreCase(String postTitle);
 }
